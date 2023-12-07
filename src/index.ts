ec779667f576be008340b6ed8f0745bed3ed18fe
Nicolly Reis <nicollyro2004@gmail.com>
2023-12-06 21:45:02 -0300

    Adiciona estruturas de rotasda aplicação

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import  express  from "express";
 import cors from "cors";
+import routesV1 from "./routes/routesV1";
 
 const app = express();
 
@@ -9,13 +10,20 @@ app.use(
   })
 );
 
+app.use("/api/v1",routesV1);
+
+app.get("/",(req,res) => {
+    res.send("Hello World!");
+});
+
 app.use((req,res) => {
     res.status(404);
+    res.send("Rota não encontrada!!!")
 });
 
 app.listen(process.env.PORT,() => {
     console.log(
-        `Servdor execuando em: ${process.env.HOST}: ${process.env.PORT}`
+        `Servdor executando em: ${process.env.HOST}: ${process.env.PORT}`
     );
 });
 
